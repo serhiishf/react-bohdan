37d55ff64cbc6d32a6e346df716b7cac3ed84a58
serhiishf <unison447@gmail.com>
2024-01-11 15:50:44 +0200

    feat: Finished precourse React

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
-const App = () => {
-  const [buttonText, setButtonText] = React.useState('Click me');
+const App = ({ initialButtonText, initialClassList }) => {
+  const [buttonText, setButtonText] = React.useState(initialButtonText);
+  const [classelList, setclasselList] = React.useState(initialClassList);
   const onButtonCLick = () => {
-    buttonText = 'Hello from React';
-    console.log(buttonText);
+    setButtonText('Hello from React');
+    setclasselList('green-btn');
   };
   return (
     <div className="app">
-      <button onClick={onButtonCLick}>{buttonText}</button>
+      <button className={classelList} onClick={onButtonCLick}>
+        {buttonText}
+      </button>
     </div>
   );
 };
 
 const container = document.getElementById('react-app');
 const root = ReactDOM.createRoot(container);
-root.render(<App />);
+root.render(<App initialButtonText="Click me new" initialClassList="" />);
