5df84e232fdfcbd7c2cb4d4d78afbef32411fc15
Serhii Shf <unison447@gmail.com>
2024-01-13 16:37:39 +0200

    feat: Finished Reusable component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import './App.css';
+import MyComponent2 from './components/MyComponent';
+import OtherComponent from './components/OtherComponent';
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <MyComponent2/>
+    <OtherComponent/>
+    <MyComponent2/>
+    <OtherComponent/>
+    <MyComponent2/>
+  </div>;
 }
 
 export default App;
---
+++ src/components/OtherComponent.jsx
@@ -0,0 +1,9 @@
+function OtherComponent () {
+  return (
+    <div>
+      <p>Paragraph inside div</p>
+    </div>
+  )
+}
+
+export default OtherComponent;
