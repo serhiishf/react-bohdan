6163eef4fecb01c6852e2f5bfd07577705e355fb
Serhii Shf <unison447@gmail.com>
2024-01-13 21:17:17 +0200

    feat: Finished 10-condition-rendering

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import PetInfo from './components/PetInfo';
 
 function App() {
   return <div className="App">
-    <PetInfo animal="cat" age="4" />
-    <PetInfo animal="elephant" age="42" />
+    <PetInfo animal="cat" age="4" hasPet={true}/>
+    <PetInfo animal="elephant" age="42" hasPet={false}/>
   </div>;
 }
 
--- src/components/PetInfo.jsx
+++ src/components/PetInfo.jsx
@@ -1,7 +1,8 @@
-function PetInfo({animal, age}) {
-  return (
+function PetInfo({animal, age, hasPet}) {
+  return hasPet ? (
     <h1>My {animal} is {age} years old</h1>
-  )
+  ) :
+  ( <h2>I don`t have an animal</h2> )
 }
 
 export default PetInfo
