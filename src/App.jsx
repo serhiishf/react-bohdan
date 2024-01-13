0a6bd22e85b5ad774a0a32c8d44a0176019c8991
Serhii Shf <unison447@gmail.com>
2024-01-13 17:12:03 +0200

    feat: Finished Props project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import './App.css';
+import PetInfo from './components/PetInfo';
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <PetInfo animal="cat" age="4" />
+    <PetInfo animal="elephant" age="42" />
+  </div>;
 }
 
 export default App;
