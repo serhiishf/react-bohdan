b82cf9cd02996ae7a3384b72b674118c7298a44a
serhiishf <unison447@gmail.com>
2024-01-18 15:54:41 +0200

    feat: Save progress

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,18 +1,25 @@
 function Login() {
+  function handleFormSubmit(event) {
+    event.preventDefault();
+    console.log(event.target);
+  }
+
   return (
     <>
-    <h1>Login form</h1>
-    <form >
-      <label>Username:
-        <input type="text" name="username"/>
-      </label>
-      <label>Password:
-        <input type="password" name="passwrod"/>
-      </label>
-      <button type="submit">Login</button>
-    </form>
+      <h1>Login form</h1>
+      <form onSubmit={handleFormSubmit}>
+        <label>
+          Username:
+          <input type="text" name="username" />
+        </label>
+        <label>
+          Password:
+          <input type="password" name="passwrod" />
+        </label>
+        <button type="submit">Login</button>
+      </form>
     </>
-  )
+  );
 }
 
 export default Login;
