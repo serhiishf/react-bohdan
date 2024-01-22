c5a788f37510d5c94f5b5220c6fd7a1cc05b9c55
Serhii Shf <unison447@gmail.com>
2024-01-22 18:37:53 +0200

    feat: Finished 20-use-effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
   const [todo, setTodo] = useState(null);
-  console.log('App render');
-  fetch('https://jsonplaceholder.typicode.com/todos/1')
+
+  useEffect(() => {
+    console.log('Callback in useEffect called')
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
     .then((response) => response.json())
     .then((json) => setTodo(json));
-  return <div className="App"></div>;
+  }, [])
+
+  console.log('App render');
+  console.log(todo);
+  return <div className="App">
+    {todo && <h1>{todo.title}</h1>}
+  </div>;
 }
 
 export default App;
