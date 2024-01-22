c3892d1901843138d5783d15130ec12caef8aa0d
serhiishf <unison447@gmail.com>
2024-01-22 16:17:07 +0200

    feat: Save progress 20-use-effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todo, setTodo] = useState(null);
+  console.log('App render');
+  fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then((response) => response.json())
+    .then((json) => setTodo(json));
   return <div className="App"></div>;
 }
 
