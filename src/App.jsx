630fa6bac37ff7a537c722b717ab6a2c8e647030
serhiishf <unison447@gmail.com>
2024-01-24 15:27:29 +0200

    feat: Finished 23-css-modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import './App.css';
+import Info from './components/Info';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Info />
+      <h1>App component heading</h1>
+      <h2>Heading in the component App</h2>
+      <button className="my-button">App component button</button>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/info.jsx
@@ -0,0 +1,17 @@
+import './Info.css';
+import styles from './Info.module.css';
+
+console.log(styles);
+
+function Info() {
+  return (
+    <div className={styles.info}>
+      <h1>Hello from Info component</h1>
+      <h2>Heading in the Info component H2</h2>
+      <button className={styles.myOtherButton}>Another button</button>
+      <button className="my-button">Click me in the Info component</button>
+    </div>
+  );
+}
+
+export default Info;
