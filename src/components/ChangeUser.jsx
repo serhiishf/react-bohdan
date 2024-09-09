704a028b1c198a8033572654d223fd23ad34e238
Serhii Shf <unison447@gmail.com>
2024-09-09 21:23:50 +0300

    feat: finished homework for 27-context-api

--- src/components/ChangeUser.jsx
+++ src/components/ChangeUser.jsx
@@ -1,9 +1,17 @@
 import UserContext from '../context/UserContext';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 
 function ChangeUser() {
   const { userName, changeUserName } = useContext(UserContext);
-  return <button onClick={() => changeUserName(userName === 'Bohdan' ? 'Alice' : 'Bohdan')}>Change user name </button>;
+  const [ inputValue, setInputValue ] = useState('')
+  console.log(inputValue)
+  return (
+    <div>
+      <input type="text" value={inputValue} onChange={(event) => setInputValue(event.target.value)}/>
+      <button onClick={() => changeUserName(inputValue)}>Change user name</button>
+    </div>
+  )
+  ;
 }
 
 export default ChangeUser;
